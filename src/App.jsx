2ba0b6a19dc0d04ffe27dbb37f09e669dc6d3a63
Kimoy <47773040+Kimoy-lomod@users.noreply.github.com>
2023-04-10 23:24:46 +0800

    hide components

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ import './css/style.css';
 import AOS from 'aos';
 
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import ResetPassword from './pages/ResetPassword';
+// import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
+// import ResetPassword from './pages/ResetPassword';
 
 function App() {
 
@@ -38,9 +38,9 @@ function App() {
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
+        {/* <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} /> */}
       </Routes>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import Header from '../partials/Header';
-import PageIllustration from '../partials/PageIllustration';
-import HeroHome from '../partials/HeroHome';
-import FeaturesBlocks from '../partials/FeaturesBlocks';
-import FeaturesZigZag from '../partials/FeaturesZigzag';
-import Testimonials from '../partials/Testimonials';
-import Newsletter from '../partials/Newsletter';
-import Banner from '../partials/Banner';
-import Footer from '../partials/Footer';
+import Header from "../partials/Header";
+import PageIllustration from "../partials/PageIllustration";
+import HeroHome from "../partials/HeroHome";
+import FeaturesBlocks from "../partials/FeaturesBlocks";
+import FeaturesZigZag from "../partials/FeaturesZigzag";
+import Testimonials from "../partials/Testimonials";
+import Newsletter from "../partials/Newsletter";
+import Banner from "../partials/Banner";
+import Footer from "../partials/Footer";
 
 function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
       {/*  Site header */}
-      <Header />
+      {/* <Header /> */}
 
       {/*  Page content */}
       <main className="grow">
@@ -25,16 +25,16 @@ function Home() {
 
         {/*  Page sections */}
         <HeroHome />
-        <FeaturesBlocks />
-        <FeaturesZigZag />
+        {/* <FeaturesBlocks />
+        <FeaturesZigZag /> */}
         <Testimonials />
-        <Newsletter />
+        {/* <Newsletter /> */}
       </main>
 
       <Banner />
 
       {/*  Site footer */}
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
